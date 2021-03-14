eccf49c07a79ea8c283657fecdea8449696af6ef
Saul Henriquez <saul_henriquez@hotmail.com>
2021-03-13 23:03:57 -0600

    release version 1.1.10

--- colorpicker-library/src/main/java/com/saggitt/colorpickerlib/OnChooseColorListener.java
+++
@@ -1,7 +0,0 @@
-package com.saggitt.colorpickerlib;
-
-public interface OnChooseColorListener {
-    void onChooseColor(int position, int color);
-
-    void onCancel();
-}
--- colorpicker-library/src/main/java/com/saggitt/colorpickerlib/OnFastChooseColorListener.java
+++
@@ -1,7 +0,0 @@
-package com.saggitt.colorpickerlib;
-
-public interface OnFastChooseColorListener {
-    void setOnFastChooseColorListener(int position, int color);
-
-    void onCancel();
-}
